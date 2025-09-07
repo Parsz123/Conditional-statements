14a2262bbf118b2e7170f381b1f02d0e515b513d
Parsz123 <hetaldr@yahoo.com>
2025-09-07 15:12:20 +0100

    hello

---
+++ activity3.py
@@ -0,0 +1,8 @@
+num1 = int(input("Please enter a number:"))
+if num1 > 15:
+    print(num1, "is greater than 15")
+    print("i'm in if block")
+else:
+    print(num1, "is not greater than 15")
+    print("i'm in else block")
+print("i'm not in if or else block")
